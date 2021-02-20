39660f5bd3785fc09ca2c9dcd58168983615a7f7
Jethet <henriette.hettinga@protonmail.com>
2021-02-20 20:25:19 +0100

    Create homepage endpoint

--- NodeGirls/server.js
+++ NodeGirls/server.js
@@ -2,6 +2,10 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", (req, res) => {
+  res.send("Homepage")
+})
+
 
 app.listen(3000, function () {
   console.log('Server running on port 3000.');
