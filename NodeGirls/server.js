da9cd4ec95b1e28ee4c3650d912427c225d2d9db
Jethet <henriette.hettinga@protonmail.com>
2021-02-21 13:52:39 +0100

    Add express-formidable, create post endpoint

--- NodeGirls/server.js
+++ NodeGirls/server.js
@@ -1,11 +1,14 @@
 const express = require("express")
+const formidable = require('express-formidable');
 
 const app = express()
 
 app.use(express.static("public"))
+app.use(formidable());
 
-
-
+app.post('/create', (req, res) => {
+  console.log("Create a post");
+})
 
 
 app.listen(3000, function () {
