2cd9ee1ca7b5ee8f8bfe86845bb24c06d6741631
Jethet <henriette.hettinga@protonmail.com>
2021-02-23 17:46:37 +0100

    Test fs read and write functionality

--- NodeGirls/server.js
+++ NodeGirls/server.js
@@ -1,22 +1,27 @@
-var fs = require('fs');
-const express = require("express")
-const formidable = require('express-formidable');
+var fs = require("fs");
+const express = require("express");
+const formidable = require("express-formidable");
 
+const app = express();
 
-const app = express()
-
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.use(formidable());
 
-fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-  console.log(file);
+app.post("/create", (req, res) => {
+  console.log(req.fields);
 });
 
-app.post('/create', (req, res) => {
-  console.log(req.fields);
-  })
 
+// let blogDB = fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+//   var parsedFile = JSON.parse(file);
+//   blogDB.add(parsedFile);
+//   return blogDB
+// });
+
+// fs.writeFile(__dirname + '/data/posts.json', data, function (error) {
+//   console.log("Data writtne to file");
+// });
 
 app.listen(3000, function () {
-  console.log('Server running on port 3000.');
-})
+  console.log("Server running on port 3000.");
+});
