b9fcac80d94eff6a26cb3458af1d58b78daab606
Jethet <henriette.hettinga@protonmail.com>
2021-02-23 16:59:38 +0100

    Add database file and readFile

--- NodeGirls/server.js
+++ NodeGirls/server.js
@@ -1,10 +1,16 @@
+var fs = require('fs');
 const express = require("express")
 const formidable = require('express-formidable');
+
 
 const app = express()
 
 app.use(express.static("public"))
 app.use(formidable());
+
+fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+  console.log(file);
+});
 
 app.post('/create', (req, res) => {
   console.log(req.fields);
