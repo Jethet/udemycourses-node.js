a8e77240edb45bea9202ad284fa48e42b6633a41
Jethet <henriette.hettinga@protonmail.com>
2021-02-25 17:35:12 +0100

    Add get posts endpoint

--- NodeGirls/server.js
+++ NodeGirls/server.js
@@ -1,26 +1,39 @@
-var fs = require("fs");
+const fs = require("fs");
 const express = require("express");
 const formidable = require("express-formidable");
+const posts = require("./data/posts.json");
+
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.static('data'));
 app.use(formidable());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.post("/create", (req, res) => {
-  console.log(req.fields);
+app.get('/posts', function (req, res) {
+  console.log("Get the posts")
+  res.sendFile(__dirname + '/data/posts.json');
 });
 
+app.post("/create", function (req, res) {
+  var blogpost = req.fields.blogpost;
 
-// let blogDB = fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-//   var parsedFile = JSON.parse(file);
-//   blogDB.add(parsedFile);
-//   return blogDB
-// });
+  fs.readFile(__dirname + "/data/posts.json", function (error, file) {
+    var parsedFile = JSON.parse(file);
+    parsedFile[Date.now()] = blogpost;
+    var stringifiedFile = JSON.stringify(parsedFile, null, 4);
 
-// fs.writeFile(__dirname + '/data/posts.json', data, function (error) {
-//   console.log("Data writtne to file");
-// });
+    fs.writeFile(__dirname + "/data/posts.json", stringifiedFile, function (error) {
+      if (error) {
+        console.error(error);
+      }
+      res.send({ blogpost: blogpost });
+    });
+  });
+});
+
 
 app.listen(3000, function () {
   console.log("Server running on port 3000.");
